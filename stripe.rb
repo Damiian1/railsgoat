d5126a729f4c5eda6069f8fd0b209d94ad234200
Damiian1 <34922879+Damiian1@users.noreply.github.com>
2019-04-13 10:40:11 +0800

    Update stripe.rb

--- stripe.rb
+++ stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe = {
-  :publishable_key =>  'pk_live_3cC20Ua0Rt6Sgitt1ltPeEA1',
-  :secretkey => 'sk_live_0aASXG1s0xABCp0k5d8AsbaL'
+  :publishable_key => ENV['PUBLISHABLE_KEY'],
+  :secretkey => 'ENV['SECRET_KEY']'
 }
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
